d4564143b2105e4f9e2a7d881bd57316a5451ae6
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-09 11:52:53 +0800

    Update TeamRoster.vue

--- resources/js/Pages/Teams/Module/TeamRoster.vue
+++ resources/js/Pages/Teams/Module/TeamRoster.vue
@@ -292,7 +292,7 @@
                         v-else
                         class="hover:bg-gray-100"
                     >
-                        <td class="px-2 py-1 whitespace-nowrap border text-center font-bold text-red-500" colspan="20">***No Players Found***</td>
+                        <td class="px-2 py-1 whitespace-nowrap border text-center font-bold text-red-500" colspan="21">***No Players Found***</td>
                     </tr>
                 </tbody>
             </table>
