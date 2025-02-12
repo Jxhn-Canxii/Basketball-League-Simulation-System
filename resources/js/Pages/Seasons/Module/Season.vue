2b6d3a2d6bfb62580626b1edea2d2edc6c249768
Jxhn-Canxii <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-12 13:39:58 +0800

    Update Season.vue

--- resources/js/Pages/Seasons/Module/Season.vue
+++ resources/js/Pages/Seasons/Module/Season.vue
@@ -46,7 +46,7 @@
             <!-- Standings UI (Left Side) -->
             <div class="overflow-y-auto">
                 <Standings v-if="updateKey" :key="updateKey" :showLegend="season_info.seasons[0].status > 2" :season_id="props.season_id" :conference_id="activeConferenceTab" :season_data="season_info.seasons" />
-                <small>Transaction ID:{{ updateKey }}</small>   
+                <small class="text-gray-500">Transaction ID:{{ updateKey }}</small>   
             </div>
             <!-- Schedule and Results UI (Right Side) -->
             <div class="md:col-span-2 sm:col-span-1 overflow-y-auto pt-3">
