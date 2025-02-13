c5bfabdc9d43c5ae5569f649101d2679a84b9704
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-13 21:59:51 +0800

    Update SeasonSchedule.vue

--- resources/js/Pages/Seasons/Module/SeasonSchedule.vue
+++ resources/js/Pages/Seasons/Module/SeasonSchedule.vue
@@ -336,6 +336,13 @@
                         await fetchConferenceSchedules();
                     }
                 } catch (error) {
+                    isHide.value = false;
+                    hasPendingGames = false;
+                    Swal.fire({
+                        icon: "success",
+                        title: "All games simulated!",
+                        text: error.response.data.message,
+                    });
                     console.error("Error fetching or simulating games:", error);
                 }
             }
