dd52dcb92dbb22108c0a46425ba75867111f2da2
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-23 14:56:13 +0800

    Update TradeController.php

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -208,6 +208,12 @@ class TradeController extends Controller
     
                             $tradeMessage = 'Trade Accepted.';
 
+                            $storeStats = new AwardsController;
+
+                            $storeStats->storePlayerSeasonStats($proposal->team_to_id, $proposal->player_from_id);
+                            $storeStats->storePlayerSeasonStats($proposal->team_from_id, $proposal->player_to_id);
+
+
                             $this->logTrade($proposal->team_to_id, $proposal->team_from_id, $proposal->player_to_id, $proposal->player_from_id, $tradeMessage, $isOffSeason);
                             $this->logTrade($proposal->team_from_id, $proposal->team_to_id, $proposal->player_from_id, $proposal->player_to_id, $tradeMessage, $isOffSeason);
                             
