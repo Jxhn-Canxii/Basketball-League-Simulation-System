8632d8fa9b96c7304664e3e67e1000abd3f90ee0
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-25 12:40:59 +0800

    uPDATEQK LFJASL

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,8 @@ Route::middleware('auth')->group(function () {
         Route::post('team-transaction-history', [TeamsController::class, 'teamsTransactionHistory'])->name('teams.transaction.history');
         Route::post('team-match-history', [TeamsController::class, 'matchHistory'])->name('match.history');
         Route::post('team-recent-season-performance', [TeamsController::class, 'countTeamOnePicksAndCheckChampion'])->name('team.recent.performance');
+        Route::post('team-match-history', [TeamsController::class, 'getTeamsByConference'])->name('conference.team.dropdown');
+        
     });
 
     Route::prefix('simulate/')->group(function(){
