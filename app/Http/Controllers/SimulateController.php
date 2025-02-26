5096fc3d4e5725ef7cdf36fa6902f4c0b0ab106f
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-02-26 19:39:53 +0800

    Update SimulateController.php

--- app/Http/Controllers/SimulateController.php
+++ app/Http/Controllers/SimulateController.php
@@ -2131,7 +2131,7 @@ class SimulateController extends Controller
 
         try {
             $seasonId = get_current_season_id();
-            $weekName = ($round == 0) ? 1 : ($round / 5);
+            $weekName = ($round == 0) ? 1 : ($round / 5) + 1;
             // Fetch player stats for previous season
             $stats = DB::table('player_season_stats')
                 ->join('players', 'player_season_stats.player_id', '=', 'players.id')
