75b6a8bcb0b5e2a93cdbe6680270ad46b95cb20e
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-03-13 00:39:56 +0800

    Update Index.vue

--- resources/js/Pages/Players/Index.vue
+++ resources/js/Pages/Players/Index.vue
@@ -150,6 +150,8 @@ const roleClasses = (role) => {
             return "bg-blue-100 text-blue-800";
         case "star player":
             return "bg-yellow-100 text-yellow-800";
+        case "all star":
+            return "bg-red-100 text-red-800";
         case "role player":
             return "bg-green-100 text-green-800";
         case "bench":
