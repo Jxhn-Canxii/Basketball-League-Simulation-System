0ab68ed2224095192e810b68905c2137bf2621c5
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-03-21 22:31:17 +0800

    Update star_players_per_season.php

--- database/view_tables/star_players_per_season.php
+++ database/view_tables/star_players_per_season.php
@@ -13,7 +13,16 @@ FROM teams;
 SET @sql = CONCAT(
     'CREATE OR REPLACE VIEW star_players_per_season AS 
     SELECT ps.season_id, ', @sql, '
-    FROM player_season_stats ps
+    FROM (
+        SELECT ps.*
+        FROM player_season_stats ps
+        INNER JOIN (
+            -- Get the latest player_season_stats.id for each player
+            SELECT player_id, MAX(id) AS latest_id
+            FROM player_season_stats
+            GROUP BY player_id
+        ) latest_stats ON ps.player_id = latest_stats.player_id AND ps.id = latest_stats.latest_id
+    ) ps
     JOIN teams t ON ps.team_id = t.id
     JOIN players p ON ps.player_id = p.id
     WHERE ps.role = ''star player''
