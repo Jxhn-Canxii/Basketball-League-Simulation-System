af7e1925c497f84d843e89e38a977a0a2cfd0bfb
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-09 10:25:11 +0800

    asdasd

--- database/alter/expand_teams.php
+++ database/alter/expand_teams.php
@@ -4,22 +4,24 @@ INSERT INTO `teams` (
     `league_id`, `conference_id`, 
     `created_at`, `updated_at`
 ) VALUES
--- Conference 1 (Philippines)
-('Bolts', 'BLT', 'Dagupan', 'Pangasinan powerhouse bringing electric plays with milkfish energy', 'FFCC00', '000000', 1, 1, NOW(), NOW()),
-('Knights', 'KNT', 'Batangas', 'Barako stronghold defenders with coffee-powered intensity', '333399', 'CCCCCC', 1, 1, NOW(), NOW()),
-('Razorbacks', 'RBK', 'Butuan', 'Agusan warriors with timber strength and riverside tenacity', '990000', '666666', 1, 1, NOW(), NOW()),
 
--- Conference 2 (China and Taiwan)
-('Ghosts', 'GHS', 'Tianjin', 'Port city phantoms with maritime stealth and industrial might', 'A9A9A9', '777777', 1, 2, NOW(), NOW()),
-('Sonics', 'SNC', 'Suzhou', 'Garden city innovators with classical precision plays', '1E90FF', '003366', 1, 2, NOW(), NOW()),
-('Cowboys', 'CBY', 'Taoyuan', 'Airport city riders with aerospace determination', 'A0522D', 'FFD700', 1, 2, NOW(), NOW()),
 
--- Conference 3 (Thailand, Indonesia, Malaysia, Vietnam)
-('Strikers', 'STR', 'Hat Yai', 'Southern Thai attackers with rubber city bounce', 'FF4500', '008000', 1, 3, NOW(), NOW()),
-('Storms', 'STM', 'Kota Kinabalu', 'Borneo tempest with Mount Kinabalu strength', '0000FF', 'B0C4DE', 1, 3, NOW(), NOW()),
-('Devil Bats', 'DBT', 'Matsuyama', 'Shikoku winged terrors with castle town spirit', '8B0000', '000000', 1, 3, NOW(), NOW()),
+-- Conference 1 (Luzon) - Moved from NCR due to city conflicts
+('Bolts', 'BLT', 'Angeles', 'Pampanga thunderbolts electrifying Clark\'s skies', 'FFCC00', '000000', 1, 1, NOW(), NOW()),
+('Knights', 'KNT', 'Malolos', 'Bulacan noble warriors with historical valor', '333399', 'CCCCCC', 1, 1, NOW(), NOW()),
+('Razorbacks', 'RBK', 'Antipolo', 'Rizal rugged hogs charging through mountainous terrain', '990000', '666666', 1, 1, NOW(), NOW()),
 
--- Conference 4 (Japan and Korea)
-('Marlins', 'MRL', 'Sendai', 'Tohoku sea hunters with samurai spirit', '4682B4', 'FF69B4', 1, 4, NOW(), NOW()),
-('Robots', 'RBT', 'Ulsan', 'Industrial automatons with shipyard strength', '808080', '2E8B57', 1, 4, NOW(), NOW()),
-('Black Bulls', 'BBL', 'Mandalay', 'Upper Myanmar chargers with ancient kingdom strength', '000000', 'B8860B', 1, 4, NOW(), NOW());
+-- Conference 2 (Luzon) - Original Luzon teams updated
+('Ghosts', 'GHS', 'Balanga', 'Bataan spectral figures haunting the peninsula\'s history', 'A9A9A9', '777777', 1, 2, NOW(), NOW()),
+('Sonics', 'SNC', 'San Fernando (LU)', 'La Union sound warriors riding the surf\'s rhythm', '1E90FF', '003366', 1, 2, NOW(), NOW()),
+('Cowboys', 'CBY', 'Lipa', 'Batangas rodeo riders with coffee-fueled energy', 'A0522D', 'FFD700', 1, 2, NOW(), NOW()),
+
+-- Conference 3 (Visayas) - Updated with new cities
+('Strikers', 'STR', 'Borongan', 'Eastern Visayas swift attackers with coastal agility', 'FF4500', '008000', 1, 3, NOW(), NOW()),
+('Storms', 'STM', 'Sipalay', 'Negros tempestuous squalls sweeping sugar fields', '0000FF', 'B0C4DE', 1, 3, NOW(), NOW()),
+('Devil Bats', 'DBT', 'Escalante', 'Northern Negros nocturnal flyers with sugarcane stealth', '8B0000', '000000', 1, 3, NOW(), NOW()),
+
+-- Conference 4 (Mindanao) - New strategic locations
+('Marlins', 'MRL', 'Koronadal', 'South Cotabato aquatic hunters thriving in Lake Sebu', '4682B4', 'FF69B4', 1, 4, NOW(), NOW()),
+('Robots', 'RBT', 'Ozamiz', 'Misamis mechanical giants with portside precision', '808080', '2E8B57', 1, 4, NOW(), NOW()),
+('Black Bulls', 'BBL', 'Gingoog', 'Misamis dark chargers with rainforest momentum', '000000', 'B8860B', 1, 4, NOW(), NOW());
