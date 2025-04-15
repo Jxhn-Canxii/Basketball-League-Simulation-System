77e05da7e54ac4900ec9f08df1ea78f7b04b9136
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-15 23:31:39 +0800

    Update TransactionsController.php

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -34,7 +34,7 @@ class TransactionsController extends Controller
             ->join('seasons', 'transactions.season_id', '=', 'seasons.id')
             ->leftJoin('teams as from_teams', 'transactions.from_team_id', '=', 'from_teams.id')
             ->leftJoin('teams as to_teams', 'transactions.to_team_id', '=', 'to_teams.id')
-            ->whereNotIn('transactions.status', ['transfer', 'role change'])
+            ->whereNotIn('transactions.status', ['transfer'])
             ->orderBy('transactions.id', 'desc')
             ->limit(5)
             ->get();
