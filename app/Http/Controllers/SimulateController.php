d8427e306f6f9253caa7719ed2dd36860e995f9d
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-16 23:55:22 +0800

    Update SimulateController.php

--- app/Http/Controllers/SimulateController.php
+++ app/Http/Controllers/SimulateController.php
@@ -1528,7 +1528,6 @@ class SimulateController extends Controller
             ->where('players.is_active', 1)
             ->where('players.is_injured', 0)
             ->where('players.team_id', 0)
-            ->where('players.role', $role)
             ->select(
                 'players.id as player_id',
                 'players.team_id',
