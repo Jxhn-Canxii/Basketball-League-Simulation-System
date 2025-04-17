16ab1c21e1f6180d6e2e770a265355de3eef2eea
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-18 03:46:14 +0800

    Update Standings.vue

--- resources/js/Pages/Seasons/Module/Standings.vue
+++ resources/js/Pages/Seasons/Module/Standings.vue
@@ -66,7 +66,11 @@
             <td class="px-4 py-2 whitespace-nowrap text-sm">{{ team.wins }}</td>
             <td class="px-4 py-2 whitespace-nowrap text-sm">{{ team.losses }}</td>
             <td class="px-4 py-2 whitespace-nowrap text-sm">
-              {{ ((parseFloat(team.total_home_score ?? 0) + parseFloat(team.total_away_score ?? 0)) / (team.wins + team.losses)).toFixed(2) }}
+              {{ 
+                (team.wins + team.losses > 0
+                  ? ((parseFloat(team.total_home_score ?? 0) + parseFloat(team.total_away_score ?? 0)) / (parseFloat(team.wins ?? 0) + parseFloat(team.losses ?? 0))).toFixed(2)
+                  : '0.00')
+              }}
             </td>
             <td class="px-4 py-2 whitespace-nowrap text-center text-sm">{{ team.overall_rank }}</td>
             <td class="px-4 py-2 whitespace-nowrap">
