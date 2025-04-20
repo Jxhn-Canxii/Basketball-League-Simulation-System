b8dc8f6a5d25e52edb3e3a49bea2e645b6f1b6b2
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-20 11:32:05 +0800

    Update SimulateController.php

--- app/Http/Controllers/SimulateController.php
+++ app/Http/Controllers/SimulateController.php
@@ -744,8 +744,8 @@ class SimulateController extends Controller
                 $rebounds = $this->calculateRebounds($player, $minutes, $performanceFactor);
                 $blocks = $this->calculateBlocks($player, $minutes, $performanceFactor);
                 $steals = $this->calculateSteals($player, $minutes, $performanceFactor);
-                $turnovers = rand(0, 2);
-                $fouls = rand(0, 4);
+                $turnovers = ($minutes === 0) ? 0 : rand(0, 2);
+                $fouls = ($minutes === 0) ? 0 : rand(0, 4);
 
                 $playerGameStats[] = [
                     'player_id' => $player->id,
