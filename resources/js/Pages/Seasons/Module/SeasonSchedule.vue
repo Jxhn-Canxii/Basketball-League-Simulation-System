54e0b60d8be229fdece71f3436f0fb651c150cb8
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-04-21 16:40:02 +0800

    Update Scorecard

--- resources/js/Pages/Seasons/Module/SeasonSchedule.vue
+++ resources/js/Pages/Seasons/Module/SeasonSchedule.vue
@@ -145,103 +145,16 @@
             </select>
         </div>
         <div
-            v-if="
-                data &&
-                data.schedules?.length > 0 && !loadingSchedules
-            "
+            v-if="data && data.schedules?.length > 0 && !loadingSchedules"
             class="grid md:grid-cols-2 sm:col-span-1 gap-6"
         >
-            <div
+            <ScoreCard
                 v-for="(game, index) in data.schedules"
                 :key="index"
-                :style="{
-                background: `
-                        linear-gradient(45deg, 
-                            #${game.home_secondary_color} 0%, 
-                            #${game.home_secondary_color} 50%, 
-                            #${game.home_primary_color} 50%, 
-                            #${game.home_primary_color} 100%
-                        ),
-                        linear-gradient(-45deg, 
-                            #${game.away_primary_color} 0%, 
-                            #${game.away_primary_color} 50%, 
-                            #${game.away_secondary_color} 50%, 
-                            #${game.away_secondary_color} 100%
-                        )`,
-                    backgroundSize: '50% 100%',
-                    backgroundPosition: 'left, right',
-                    backgroundRepeat: 'no-repeat'
-                }"
-                class="rounded"
-            >
-                <div
-                    class="px-5 text-6xl font-bold text-white py-0 flex justify-between items-center"
-                >
-                    <div>
-                        <h3>
-                            {{ game.home_score }}
-                        </h3>
-                    </div>
-                    <div>
-                        <h3>
-                            {{ game.away_score }}
-                        </h3>
-                    </div>
-                </div>
-                <div
-                    class="px-1 py-2 flex justify-between items-center"
-                >
-                    <h3>
-                        <TeamDetails
-                            :team_id="game.home_team_id" 
-                            :key="game.home_team_id"
-                            class="text-white text-xl uppercase text-wrap text-left"
-                            :showButton="0"
-                            :text="`${game.home_team_name}`" />
-                    </h3>
-                    <h3>
-                        <TeamDetails
-                            :team_id="game.away_team_id" 
-                            :key="game.away_team_id"
-                            class="text-white text-xl uppercase text-wrap text-right" 
-                            :showButton="0" 
-                            :text="`${game.away_team_name}`" />
-                    </h3>
-                    </div>
-                    <div
-                        class="px-4 text-nowrap text-xs py-0"
-                    >
-                        <span class="px-2 text-lg text-white py-1 rounded  flex justify-center">
-                            {{ "Round #" + parseFloat(game.round) }}
-                        </span>
-                        <small class="flex justify-center text-white"> #R{{ game.round }}-{{ game.game_id }}</small>
-                    </div>
-                    <div class="p-0 mt-2 flex justify-center items-center">
-                        <div
-                            v-if="!isHide"
-                        >
-                            <a
-                                href="#"
-                                class="text-xs text-blue-500 underline font-bold rounded-t bg-white p-2"
-                                @click.prevent="
-                                    isGameResultModalOpen =
-                                        game.game_id
-                                "
-                                >View Result
-                            </a>
-                        </div>
-                        <div
-                            v-else
-                            class="text-center"
-                        >
-                            <p
-                               class="text-sm text-blue-500 underline font-bold rounded bg-white p-2"
-                            >
-                                Getting results
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                :game="game"
+                :isSimulating="isHide"
+                @view-result="isGameResultModalOpen = $event"
+            />
         </div>
         <div v-if="loadingSchedules">
             <p class="text-gray-500">Loading Schedules.</p>
@@ -290,6 +203,7 @@
     import TeamDetails from "@/Pages/Teams/Module/TeamDetails.vue";
     import Trade from "@/Pages/Seasons/Module/Trade.vue";
     import RecentTransactions from "@/Pages/Seasons/Module/RecentTransactions.vue";
+    import ScoreCard from "@/Pages/Seasons/Module/ScoreCard.vue";
 
     const season_schedules = ref(false);
     const isTradeModalOpen = ref(false);
