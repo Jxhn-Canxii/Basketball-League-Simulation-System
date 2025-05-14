6b7af405e6474bbb4484cef233922cfae7a11ba7
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-05-15 07:26:41 +0800

    Update Playoffs.vue

--- resources/js/Pages/Seasons/Module/Playoffs.vue
+++ resources/js/Pages/Seasons/Module/Playoffs.vue
@@ -857,8 +857,8 @@ const simulateFullPlayoffs = async () => {
             'play_ins_elims_round_2',
             'play_ins_finals',
             'round_of_16',
-            'quarterfinals',
-            'semifinals',
+            'quarter_finals',
+            'semi_finals',
             'interconference_semi_finals',
             'finals'
         ];
