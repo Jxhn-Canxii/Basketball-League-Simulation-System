1850dc3c8c6bcbe2b1560f824f89cf45e2b1800d
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-05-21 22:38:34 +0800

    Update PlayerPlayoffAppearance.vue

--- resources/js/Pages/Experience/Module/PlayerPlayoffAppearance.vue
+++ resources/js/Pages/Experience/Module/PlayerPlayoffAppearance.vue
@@ -57,7 +57,7 @@
                         <span
                             v-if="!player.active_status"
                             class="inline-flex items-center px-2.5 py-0.5 rounded text-xs font-medium bg-red-100 text-red-800"
-                        >Free Agent/Retired</span>
+                        >Retired</span>
                         <span
                             v-else
                             class="inline-flex items-center px-2.5 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800"
