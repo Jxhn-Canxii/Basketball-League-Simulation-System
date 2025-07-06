bf53960461a82346ffb057c74d91602ea4ab260e
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-07-06 10:20:37 +0800

    Update draft_view.php

--- database/view_tables/draft_view.php
+++ database/view_tables/draft_view.php
@@ -18,5 +18,5 @@ FROM drafts d
 JOIN players p ON d.player_id = p.id
 JOIN teams t1 ON d.team_id = t1.id
 LEFT JOIN teams t2 ON p.team_id = t2.id
-WHERE d.pick_number <= 5
+WHERE d.pick_number <= 5 && d.round = 1
 ORDER BY d.season_id DESC, d.pick_number;
