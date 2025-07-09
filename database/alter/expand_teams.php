c847596eb9fc8cbda8ab2e2e9bcb6f7d91115cbe
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-07-09 20:52:12 +0800

    The Alignment

--- database/alter/expand_teams.php
+++ database/alter/expand_teams.php
@@ -8,12 +8,12 @@ INSERT INTO `teams` (
 ('Angels', 'ACA', 'Angeles City', 'Soar with Grace!', 'FFCC00', '003366', 1, 1, NOW(), NOW()),
 ('Knights', 'KNT', 'Malolos', 'Charge with Valor!', '333399', 'CCCCCC', 1, 1, NOW(), NOW()),
 ('Rascals', 'RSC', 'West Manila', 'Hustle with Heart!', 'FFCC00', 'FF5733', 1, 1, NOW(), NOW()),
-('Guardians', 'GRD', 'Rizal', 'Defend with Might!', '004B87', 'F1C40F', 1, 1, NOW(), NOW()),
+('Stingers', 'PTS', 'Pateros', 'Small Team. Big Sting.', 'FFD700', '1A1A1A', 1, 1, NOW(), NOW())
 -- Conference 2 (Luzon)
 ('Ghosts', 'GHS', 'Bataan', 'Haunt the Court!', 'A9A9A9', '777777', 1, 2, NOW(), NOW()),
 ('Sonics', 'SNC', 'La Union', 'Ride the Rhythm!', '1E90FF', '003366', 1, 2, NOW(), NOW()),
 ('Kryptonites', 'KRY', 'Bulacan', 'Shine Unstoppable!', '3B8C3A', 'FFD700', 1, 2, NOW(), NOW()),
-('Razorbacks', 'RAZ', 'Sta. Rosa', 'Cut to Conquer!', '8B0000', 'A52A2A', 1, 2, NOW(), NOW()),
+('Crimson Arrows', 'ICA', 'Imus', 'Bleed for the Mark!', '8B0000', 'FFD700', 1, 2, NOW(), NOW()),
 -- Conference 3 (Visayas)
 ('Storms', 'STM', 'Sipalay', 'Unleash the Tempest!', '0000FF', 'B0C4DE', 1, 3, NOW(), NOW()),
 ('Elites', 'ECE', 'Escalante', 'Rise to Excellence!', '8B0000', '000000', 1, 3, NOW(), NOW()),
