bd64e099b78031c790a3e6de8acb7b801048f3a4
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-07-12 12:46:51 +0800

    sdfsdfsdf

--- database/alter/expand_teams.php
+++ database/alter/expand_teams.php
@@ -9,13 +9,13 @@ INSERT INTO `teams` (
 ('Angels',          'ACA', 'Angeles City',     'Soar with Grace!',       'FFCC00', '003366', 1, 1, 'AngelCore',         NOW(), NOW()),
 ('Knights',         'KNT', 'Malolos',          'Charge with Valor!',     '333399', 'CCCCCC', 1, 1, 'ValorArmory',       NOW(), NOW()),
 ('Rascals',         'RSC', 'West Manila',      'Hustle with Heart!',     'FFCC00', 'FF5733', 1, 1, 'RogueMotion',       NOW(), NOW()),
-('Stingers',        'PTS', 'Pateros',          'Small Team. Big Sting.', 'FFD700', '1A1A1A', 1, 1, 'NanoSting',         NOW(), NOW()),
+('Ducks',        'PTS', 'Pateros',          'One Quack!,One Team!', 'FFD700', '1A1A1A', 1, 1, 'Duckling Foundation',         NOW(), NOW()),
 
 -- Conference 2 (Luzon)
 ('Ghosts',          'GHS', 'Bataan',           'Haunt the Court!',       'A9A9A9', '777777', 1, 2, 'PhantomWare',       NOW(), NOW()),
 ('Sonics',          'SNC', 'La Union',         'Ride the Rhythm!',       '1E90FF', '003366', 1, 2, 'EchoDrive',         NOW(), NOW()),
 ('Kryptonites',     'KRY', 'Bulacan',          'Shine Unstoppable!',     '3B8C3A', 'FFD700', 1, 2, 'GreenStar Labs',    NOW(), NOW()),
-('Crimson Arrows',  'ICA', 'Imus',             'Bleed for the Mark!',    '8B0000', 'FFD700', 1, 2, 'ArrowForge',        NOW(), NOW()),
+('Red Arrows',  'IRA', 'Imus',             'Bleed for the Mark!',    '8B0000', 'FFD700', 1, 2, 'ArrowForge',        NOW(), NOW()),
 
 -- Conference 3 (Visayas)
 ('Storms',          'STM', 'Sipalay',          'Unleash the Tempest!',   '0000FF', 'B0C4DE', 1, 3, 'TempestCore',       NOW(), NOW()),
