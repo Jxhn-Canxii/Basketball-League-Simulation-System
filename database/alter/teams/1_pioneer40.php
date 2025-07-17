21caa1b8770a5913e157990ef0d27a8c797fef1b
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-07-18 07:23:23 +0800

    Update 1_pioneer40.php

--- database/alter/teams/1_pioneer40.php
+++ database/alter/teams/1_pioneer40.php
@@ -47,13 +47,13 @@ INSERT INTO `teams` (`id`, `name`, `acronym`, `primary_color`, `secondary_color`
 INSERT INTO `teams` (`id`, `name`, `acronym`, `primary_color`, `secondary_color`,`league_id`, `conference_id`,`city`, `description`, `sponsor`, `created_at`, `updated_at`) VALUES
 (49, 'Pirates', 'PIR', 'FFB612', '34302B', 1, 4, 'Davao City', 'Plunder the Prize!', 'Skullwave', NOW(), NOW()),
 (50, 'Scorpions', 'SCR', '8A4F00', 'FFD700', 1, 4, 'Zamboanga', 'Sting with Force!', 'Scorpex Systems', NOW(), NOW()),
-(51, 'Thunderhawks', 'THK', '1A1A2E', 'FFD700', 1, 4, 'Cagayan de Oro', 'Strike from Above!', 'VoltEdge Energy', NOW(), NOW())
+(51, 'Thunderhawks', 'THK', '1A1A2E', 'FFD700', 1, 4, 'Oro', 'Strike from Above!', 'VoltEdge Energy', NOW(), NOW())
 (52, 'Reapers', 'RPR', '1B1B1B', 'D50000', 1, 4, 'General Santos', 'Harvest the Win!', 'Darkharvest Ltd.', NOW(), NOW()),
 (53, 'Raiders', 'RAI', '000000', 'A5ACAF', 1, 4, 'Butuan', 'Seize the Victory!', 'BlackFlag Co.', NOW(), NOW()),
 (54, 'Whales', 'WH', '005C5C', 'A1D6E2', 1, 4, 'Tagum', 'Dive to Dominate!', 'OceanCore', NOW(), NOW()),
 (55, 'Poseidons', 'POS', '003B4F', '00B4D8', 1, 4, 'Surigao City', 'Command the Tides!', 'Trident Co.', NOW(), NOW()),
 (56, 'Cyclones', 'CYC', '4B0082', 'FFD700', 1, 4, 'Cotabato City', 'Spin to Glory!', 'StormVibe Inc.', NOW(), NOW()),
-(57, 'Force', 'FRC', '006BB6', 'ED174C', 1, 4, 'Zamboanga del Sur','Charge with Power!', 'CoreForce', NOW(), NOW()),
-(58, 'Astronauts', 'AST', '1A1A40', 'F28C28', 1, 4, 'Lanao del Sur', 'Orbit to Victory!', 'Orbitek Systems', NOW(), NOW()),
+(57, 'Force', 'FRC', '006BB6', 'ED174C', 1, 4, 'Pagadian City','Charge with Power!', 'CoreForce', NOW(), NOW()),
+(58, 'Astronauts', 'AST', '1A1A40', 'F28C28', 1, 4, ''Kidapawan City', 'Orbit to Victory!', 'Orbitek Systems', NOW(), NOW()),
 (59, 'Demons',    'DMN', '0B162A', 'FF3C00', 1, 4, 'Iligan City',  'Unleash the Chaos!', 'Hellspawn Gear',NOW(), NOW()),
 (60, 'Devils',    'DVL', 'D50A0A', '000000', 1, 4, 'Malaybalay',   'Burn with Passion!', 'Infernal Works',NOW(), NOW());
