e43ce53d4bbc6ed00958956de90e82a38aac320f
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-08-09 22:09:56 +0800

    asdad

--- database/view_tables/rank_coach_by_championship.php
+++ database/view_tables/rank_coach_by_championship.php
@@ -1,13 +1,16 @@
 CREATE OR REPLACE VIEW coach_championships AS
-SELECT 
+SELECT
     c.id AS coach_id,
     c.name AS coach_name,
     COUNT(*) AS total_championships,
-    GROUP_CONCAT(CONCAT(s.name, ' - ', t.name) ORDER BY s.id SEPARATOR ', ') AS championship_seasons_and_teams
+    GROUP_CONCAT(
+        CONCAT(s.name, ' - ', IFNULL(t.name, 'Unknown Team'))
+        ORDER BY s.id SEPARATOR ', '
+    ) AS championship_seasons_and_teams
 FROM team_season_info tsi
 JOIN coaches c ON c.id = tsi.coach_id
 JOIN teams t ON t.id = tsi.team_id
 JOIN seasons s ON s.id = tsi.season_id
-WHERE tsi.is_defending_champion = 1
+WHERE tsi.team_id = s.finals_winner_id
 GROUP BY c.id, c.name
-ORDER BY total_championships DESC, c.name;
+ORDER BY total_championships DESC, s.id DESC,c.name;
