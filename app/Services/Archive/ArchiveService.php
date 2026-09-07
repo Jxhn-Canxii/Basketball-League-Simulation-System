b6067ce5608673b0f7d3328e61b5f6c32353b689
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2026-09-08 00:33:29 +0800

    Update ArchiveService.php

--- app/Services/Archive/ArchiveService.php
+++ app/Services/Archive/ArchiveService.php
@@ -340,7 +340,7 @@ class ArchiveService
             }
 
             //schedules table must left previous season record and current record for streak tracking
-            DB::statement("DELETE FROM schedules WHERE season_id < $currentSeasonId - 1");
+            DB::statement("DELETE FROM schedules WHERE season_id < ($currentSeasonId - 1)");
 
             DB::commit();
         } catch (\Exception $e) {
