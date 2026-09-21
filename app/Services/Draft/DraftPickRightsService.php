6e264442295ef751e9594fd4e65f88b6c5f945a0
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2026-09-21 08:11:36 +0800

    asdasd

--- app/Services/Draft/DraftPickRightsService.php
+++ app/Services/Draft/DraftPickRightsService.php
@@ -44,9 +44,9 @@ class DraftPickRightsService
             return;
         }
 
-        $draftRounds  = $currentSeasonId > 0 ? [1,2] : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
         foreach ($futureSeasons as $seasonId) {
+
+            $draftRounds = $seasonId > 1 ? [1,2] : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
             foreach ($teams as $team) {
 
