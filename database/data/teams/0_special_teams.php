d22e566168e05e945ef0fa4a3edbdec95f2b4ae3
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2026-09-21 21:24:19 +0800

    Update 0_special_teams.php

--- database/data/teams/0_special_teams.php
+++ database/data/teams/0_special_teams.php
@@ -1,6 +1,6 @@
 -- NCR (Conference 1) - Initial 12
 INSERT INTO `teams` (`name`, `acronym`, `primary_color`, `secondary_color`, `league_id`, `conference_id`, `city`, `description`, `sponsor`, `created_at`, `updated_at`) VALUES
-('All-Stars', 'NORTH', '0076B6', 'B0B7BC', 1, 0, 'North', 'All-Star select for North', 'Maharlika.Inc', NOW(), NOW()),
-('All-Stars', 'SOUTH', '0C2340', 'FA4616', 1, 0, 'South', 'All-Star select for South', 'Maharlika.Inc', NOW(), NOW()),
-('All-Rookies', 'NORTH-R', '0B162A', 'C83803', 1, 0, 'North','All-Rookie select for North', 'Maharlika.Inc', NOW(), NOW()),
-('All-Rookies', 'SOUTH-R', '236192', 'C4CED4', 1, 0, 'South', 'All-Rookie select for South', 'Maharlika.Inc', NOW(), NOW());
+('North All-Stars', 'NORTH', '0076B6', 'B0B7BC', 1, 0, 'Maharlika', 'All-Star select for North', 'Maharlika.Inc', NOW(), NOW()),
+('South All-Stars', 'SOUTH', '0C2340', 'FA4616', 1, 0, 'Maharlika', 'All-Star select for South', 'Maharlika.Inc', NOW(), NOW()),
+('North All-Rookie Selection', 'NORTH-R', '0B162A', 'C83803', 1, 0, 'Maharlika','All-Rookie select for North', 'Maharlika.Inc', NOW(), NOW()),
+('South All-Rookie Selection', 'SOUTH-R', '236192', 'C4CED4', 1, 0, 'Maharlika', 'All-Rookie select for South', 'Maharlika.Inc', NOW(), NOW());
